d6275ff0943fdef1ce67248bc2f35e615edd048a
amira asfan <meroo.asfan2@gmail.com>
2019-12-09 05:30:15 +0200

    inital commit

---
+++ hello2.js
@@ -0,0 +1,21 @@
+var http =require('http');
+var fs =require('fs');
+var url=require('url');
+
+http.createServer(function(req,res){
+    var q=url.parse(req.url,true);
+    var filename="."+q.pathname;
+    if (filename =='./'){filename='./sample';}
+    filename=filename +".html";
+    fs.readFile(filename,function(err, data){
+        if (err){
+            res.writeHead(404,{'content-type':'text/html'});
+            return res.end("404 Not Found");
+        }
+        res.writeHead(200,{'content-type':'text/html'});
+        res.write(data);
+        console.log("...incoming" + req.url);
+        return res.end();
+    })
+}).listen(8081);
+console.log("server is listening on port 8081");
